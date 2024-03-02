80296de1dbdd1cb5af69b9dc7fb97b7be742f7a6
faizttn <faijuddin@tothenew.com>
2024-03-02 14:14:12 +0530

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import MuiTypography from './components/MuiTypography';
+import MuiBotton from './components/MuiBotton';
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
+      {/* <MuiTypography /> */}
+      <MuiBotton />
     </div>
   );
 }
---
+++ src/components/MuiBotton.tsx
@@ -0,0 +1,39 @@
+import { Button, Stack, IconButton } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
+
+const MuiBotton = () => {
+    return (
+        <Stack spacing={4}>
+            <Stack spacing={2} direction={'row'}>
+                <Button variant='text' href='https://google.com'>Text</Button>
+                <Button variant='contained'>contained</Button>
+                <Button variant='outlined'>Outlined</Button>
+            </Stack>
+
+            <Stack spacing={2} direction={'row'}>
+                <Button variant='contained' color='primary'>Primary</Button>
+                <Button variant='contained' color='secondary'>secondary</Button>
+                <Button variant='contained' color='error'>error</Button>
+                <Button variant='contained' color='warning'>warning</Button>
+                <Button variant='contained' color='info'>info</Button>
+                <Button variant='contained' color='success'>success</Button>
+            </Stack>
+
+            <Stack display={'block'} spacing={2} direction={'row'}>
+                <Button variant='contained' size='small'>Small</Button>
+                <Button variant='contained' size='medium'>medium</Button>
+                <Button variant='contained' size='large'>Large</Button>
+            </Stack>
+
+            <Stack spacing={2} direction={'row'}>
+                <Button variant='contained' startIcon={<SendIcon></SendIcon>} disableRipple onClick={() => alert('Hello')}>Send</Button>
+                <Button variant='contained' endIcon={<SendIcon></SendIcon>} disableElevation>Send</Button>
+                <IconButton aria-label='send' color='success' size='small'>
+                    <SendIcon />
+                </IconButton>
+            </Stack>
+        </Stack>
+    )
+}
+
+export default MuiBotton
