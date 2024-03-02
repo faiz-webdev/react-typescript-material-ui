f7713a6f027bedb1c9c7d638a68d79315ceba138
faizttn <faijuddin@tothenew.com>
2024-03-02 22:38:03 +0530

    textfield

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './App.css';
 import MuiTypography from './components/MuiTypography';
 import MuiBotton from './components/MuiBotton';
 import MuiBox from './components/MuiBox';
+import MuiTextField from './components/MuiTextField';
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography /> */}
-      <MuiBotton />
+      {/* <MuiBotton /> */}
       {/* <MuiBox /> */}
+      <MuiTextField />
     </div>
   );
 }
